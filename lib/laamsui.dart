7ae79ddcafe18f0cc00178590413c32cfba45202
Najibullah Khoda Rahim <parwandaha@gmail.com>
2024-10-24 22:11:04 +0430

    Fixed bugs and added features

--- lib/laamsui.dart
+++ lib/laamsui.dart
@@ -1,7 +1,7 @@
 /// A design system with a collection Futter UI Widgets and dart extensions.
 /// This library contains all sorts of Dart functionalities and
 /// Flutter adaptive UI [Widget]s
-library laamsui;
+library;
 
 export 'src/animations.dart';
 export 'src/laams_icons.dart';
